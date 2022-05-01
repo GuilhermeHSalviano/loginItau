77e1736befc1b6c2674fb4e9863231b0bd939a19
Guilherme <guilherme.salviano@hotmail.com>
2022-05-01 11:14:33 -0300

    first commit

---
+++ src/components/home/Home.vue
@@ -0,0 +1,14 @@
+<template>
+    <my-nav></my-nav>
+</template>
+<script>
+import MyNav from "./MyNav.vue"
+export default {
+    components:{
+        MyNav
+    }
+}
+</script>
+<style scoped>
+
+</style>
